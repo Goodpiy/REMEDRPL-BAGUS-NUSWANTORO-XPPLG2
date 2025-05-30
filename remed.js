1f2aa49a1afaf099ea04a35fd34fabc49da46e2a
Goodpiy <ajibond02@gmail.com>
2025-05-30 14:40:59 +0700

    Add files via upload

---
+++ remed.js
@@ -0,0 +1,63 @@
+function tampil(){
+    let info = document.getElementById("spek");
+    let tambah = document.getElementById("tombol");
+
+    if(info.style.display === "none" || info.style.display === "") {
+        info.style.display = "block";
+        tambah.innerText = "sembunyikan spek";
+    } else {
+        info.style.display = "none";
+        tambah.innerText = "tampilkan spek";
+    }
+}
+
+function pencet(){
+    let nama = document.getElementById('namaproduk').value.trim();
+    let harga = document.getElementById('hargaproduk').value.trim();
+    let des = document.getElementById('deskripsi').value.trim();
+
+    if (!nama || !harga || !des){
+        swal.fire({
+            icon: 'error',
+            title: 'Gagal!',
+            text: 'tidak ada yang baru!',
+        });
+        return;
+    }
+
+    swal.fire({
+        icon: 'success',
+        title: 'Yes!',
+        text: 'Berhasil memperbarui!',
+    });
+
+    let keluar = document.getElementById('output');
+    let kotak = document.createElement('div');
+    kotak.className = 'container';
+    kotak.innerHTML = `
+        <h4><strong> nama produk: </strong> ${nama}</h4>
+        <p><strong>Harga:</strong> Rp ${parseInt(harga).toLocaleString()}</p>
+        <p><strong>Deskripsi:</strong> ${des}</p>
+        <button onclick="hapussatu(this)">HAPUS</button>
+    `;
+    keluar.appendChild(kotak);
+
+    document.getElementById('namaproduk').value = '';
+    document.getElementById('hargaproduk').value = '';
+    document.getElementById('deskripsi').value = '';
+}
+
+function hapussatu(el){
+    swal.fire({
+        title: 'beneran mau di hapus?',
+        icon: 'warning!',
+        showCancelButton: true,
+        confirmButton: 'YA',
+        cancelButton: 'TIDAK JADI',
+    }).then((result) =>{
+        if (result.isConfirmed){
+            el.parentElement.remove();
+            swal.fire('mantap!', '', 'success');
+        }
+    }); 
+}
